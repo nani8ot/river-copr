d99e2b634058e2097d7ef3e829b62214010a8348
nani8ot <github@wiedemer.xyz>
2023-03-15 14:15:39 +0100

    Create river-git.sh

---
+++ river-git.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/riverwm/river
+wget https://raw.githubusercontent.com/nani8ot/river-copr/main/river-git.spec -O river.spec
+cd river
+git submodule update --init
+cd ..
+
+tar -czf river.tar.gz river-git
